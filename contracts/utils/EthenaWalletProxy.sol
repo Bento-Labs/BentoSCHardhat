d0b7c58ecea84f52e4d9d6f23b5760fd0af01145
anhdungle <anhdungle634@gmail.com>
2025-01-21 16:21:55 +0700

    natspec for EthenaWalletProxy

--- contracts/utils/EthenaWalletProxy.sol
+++ contracts/utils/EthenaWalletProxy.sol
@@ -3,24 +3,42 @@ pragma solidity 0.8.27;
 
 import {IStakedUSDe} from "../interfaces/IStakedUSDe.sol";
 // Since we cannot stake directly to Ethena from the staking vault, as then the unbonding period would be bound to one address, we use proxy to have separate unbonding periods for each user.
+
+/**
+ * @title EthenaWalletProxy
+ * @notice Since we cannot stake directly to Ethena from the staking vault, as then the unbonding period would be bound to one address, we use proxy to have separate unbonding periods for each user. We don't need to implement deposit here because we use batch deposit from the bentoUSDVault to Ethena.
+ */
 contract EthenaWalletProxy {
+    /// @notice Address of the Ethena vault (sUSDe)
+    /// @dev On Ethereum mainnet it is ssumed to be at address 0x9D39A5DE30e57443BfF2A8307A4256c8797A3497
     address public ethenaVault;
-    // ethenaVault is sUSDe (StakedUSDeV2) assumed to be at address 0x9D39A5DE30e57443BfF2A8307A4256c8797A3497
-    // due to the unbonding period of sUSDe, we cannot withdraw directly, but we need to trigger this period by calling cooldownAssets or cooldownShares first
     address public bentoUSDVault;
 
+    /**
+     * @notice Initializes the EthenaWalletProxy contract
+     * @param _ethenaVault The address of the Ethena vault
+     * @param _bentoUSDVault The address of the BentoUSD vault
+     */
     constructor(address _ethenaVault, address _bentoUSDVault) {
         ethenaVault = _ethenaVault;
         bentoUSDVault = _bentoUSDVault;
     }
     
-    // we don't need to implement deposit since we use batch deposit from the bentoUSDVault to Ethena
+    /**
+     * @notice Commits a withdrawal by triggering the cooldown period
+     * @dev Only callable by the BentoUSD vault
+     * @param _amount The amount to withdraw
+     */
     function commitWithdraw(uint256 _amount) external {
         require(msg.sender == bentoUSDVault, "Only bentoUSDVault can call withdraw");
         // there is a mitmatch between Ethena onchain code (which has only one parameter) and github repo (which has 2 parameters) for cooldownShares
         IStakedUSDe(ethenaVault).cooldownShares(_amount);
     }
 
+    /**
+     * @notice Withdraws the staked assets to a recipient
+     * @param _recipient The address to receive the unstaked assets
+     */
     function withdraw(address _recipient) external {
         IStakedUSDe(ethenaVault).unstake(_recipient);
     }
